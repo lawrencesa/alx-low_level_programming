f5a788b590bc1934b59bb25172a0e1b8eb56e53b
Lawrencesa <Lawrencesammue793@gmail.com>
2022-12-10 03:36:05 -0800

    T4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,11 @@
-#include <stdio.h>                                                                                                                                      
+#include <stdio.h>
 
-                                                                                                                                                        
-
-/**                                                                                                                                                     
-
- * main - main block                                                                                                                                    
-
- * Return: 0                                                                                                                                            
-
- */                                                                                                                                                     
-
-                                                                                                                                                        
-
-int main(void)                                                                                                                                          
-
-                                                                                                                                                        
-
-{                                                                                                                                                       
-
-        puts("\"Programming is like building a multilingual puzzle");                                                                                   
-
-        return (0);                                                                                                                                     
-
-}     
+/**
+*main - Entry Point
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like a building a multilingual puzzle");
+	return (0);
+}
