c0630c8542b79a25fc57a16d7327c03a43010654
Lawrencesa <Lawrencesammue793@gmail.com>
2022-12-10 01:08:26 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (Successs)
+ */
+
+int main(void)
+{
+	puts("\"Programming is lie building a multilingual puzzle");
+	return (0);
+
+}
