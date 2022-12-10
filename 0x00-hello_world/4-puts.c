b73491c0c43928659fb0d150bcf003083a07fbd0
Lawrencesa <Lawrencesammue793@gmail.com>
2022-12-10 03:00:58 -0800

    T4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
- *
  * main - main block 
  * Return: 0
  */
@@ -9,8 +8,6 @@
 int main(void)
 
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
